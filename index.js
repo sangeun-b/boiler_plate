c73b5fd99c8109a0787cb006e5a1df23dc3b8cbc
sangeun-b <you@example.com>
2021-01-26 15:25:50 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const port = 5000;
+const mongoose = require("mongoose");
+mongoose
+  .connect(
+    "mongodb+srv://baek:1015@boilerplate.zxp3w.mongodb.net/test?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    }
+  )
+  .then(() => console.log("MongoDB Connected..."))
+  .catch((err) => console.log(err));
+
+app.get("/", (req, res) => {
+  res.send("Hello World!!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
